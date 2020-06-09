8e46cd5da542d46d2b9e59803decc6319a1d1f59
Rafael Castillo <rafaelcastillo@outlook.com>
2020-06-08 19:46:44 -0500

    PromoActivity added.

--- app/src/main/java/com/example/emelatour/MainActivity.java
+++ app/src/main/java/com/example/emelatour/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.emelatour;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onImageClick(View view)
+    {
+        Log.d("MainActivity", "Image clicked");
+        Intent intent = new Intent(this, PromoActivity.class);
+        startActivity(intent);
     }
 }
